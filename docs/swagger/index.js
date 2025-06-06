fa96eb9ff9587741fc4c27d113ccc0c373ecfebf
Hamza Daoud <hamzadaoud99@gmail.com>
2025-06-06 07:32:41 +0300

    Update API documentation title from 'ERIDE API Documentation' to 'MOCK API Documentation'

--- docs/swagger/index.js
+++ docs/swagger/index.js
@@ -22,7 +22,7 @@ const options = {
     definition: {
         openapi: '3.0.0',
         info: {
-            title: 'ERIDE API Documentation',
+            title: 'MOCK API Documentation',
             version: '1.0.0',
             description: `## Authentication Flows
 
